2fc2aa018e811e6d2549ecf8e68b5a7ec15d42c5
Pablo Moreno <ehtomer@gmail.com>
2024-01-15 07:53:48 +0100

    opp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/**/*.{html,js}, ./*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     extend: {},
     fontFamily: {
       titles: ["Serif", "serif"],
     },
     boxShadow: {
-      center: "0 0 20px 0 #e5e5e58a",
+      center: "0 0 10px 0 #e5e5e58a",
     },
   },
   plugins: [],
